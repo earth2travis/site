7eefcae9cc7aac63dab93d9c547394e52a4138ff
earth2travis <travis.mccutcheon@gmail.com>
2023-05-17 14:06:59 -0500

    Update homepage content

--- .next/server/flight-server-css-manifest.js
+++ .next/server/flight-server-css-manifest.js
@@ -1,18 +1,1 @@
-self.__RSC_CSS_MANIFEST={
-  "cssImports": {
-    "/Users/earth/Sites/projects/e2t/site/app/page.tsx": [
-      "/Users/earth/Sites/projects/e2t/site/app/page.module.css"
-    ],
-    "/Users/earth/Sites/projects/e2t/site/app/layout.tsx": [
-      "/Users/earth/Sites/projects/e2t/site/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
-      "/Users/earth/Sites/projects/e2t/site/app/globals.css"
-    ]
-  },
-  "cssModules": {
-    "/Users/earth/Sites/projects/e2t/site/app/page": [
-      "/Users/earth/Sites/projects/e2t/site/app/page.module.css",
-      "/Users/earth/Sites/projects/e2t/site/app/globals.css",
-      "/Users/earth/Sites/projects/e2t/site/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
-    ]
-  }
-}
+self.__RSC_CSS_MANIFEST={"cssImports":{"/Users/earth/Sites/projects/e2t/site/app/page.tsx":["/Users/earth/Sites/projects/e2t/site/app/page.module.css"],"/Users/earth/Sites/projects/e2t/site/app/layout.tsx":["/Users/earth/Sites/projects/e2t/site/app/globals.css","/Users/earth/Sites/projects/e2t/site/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"]},"cssModules":{"/Users/earth/Sites/projects/e2t/site/app/page":["/Users/earth/Sites/projects/e2t/site/app/page.module.css","/Users/earth/Sites/projects/e2t/site/app/globals.css","/Users/earth/Sites/projects/e2t/site/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"]}}
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,38 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from 'next/image';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        <h1>Ξ2T</h1>
       </div>
 
       <div className={styles.grid}>
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href='https://docs.earth2travis.com/'
           className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+          target='_blank'
+          rel='noopener noreferrer'
         >
           <h2>
             Docs <span>-&gt;</span>
           </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
+          <p>Mountains of Markdown may eventually end up here</p>
         </a>
 
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href='https://blog.earth2travis.com/'
           className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+          target='_blank'
+          rel='noopener noreferrer'
         >
           <h2>
-            Learn <span>-&gt;</span>
+            Blog <span>-&gt;</span>
           </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+          <p>There may be a day when I actually write some shit</p>
         </a>
       </div>
     </main>
-  )
+  );
 }
