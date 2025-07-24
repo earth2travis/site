a98352b313df16214bcf7e556b68b6c68457ad8a
e2t <accounts@earth2travis.com>
2025-07-24 11:10:11 -0500

    Remove content from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,12 @@
-import styles from './page.module.css';
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.center}>
-        <h1>Ξ2T</h1>
-      </div>
-
       <div className={styles.grid}>
         <h2>DAO Designer</h2>
         <h2>Token Engineer</h2>
         <h2>Protocol Specialist</h2>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://docs.earth2travis.com/"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Mountains of Markdown may eventually end up here</p>
-        </a>
-
-        <a
-          href="https://blog.earth2travis.com/"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Blog <span>-&gt;</span>
-          </h2>
-          <p>There may be a day when I actually write some shit</p>
-        </a>
       </div>
     </main>
   );
